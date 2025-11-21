611815f388ec15daa099c2da5562db3b74176f61
Leonie von Schirach <leonie.schirach@web.de>
2025-11-21 12:28:15 +0000

    feat: add encode_prompt method that returns PyTorch tensor

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class Chatbot:
+    
     def __init__(self):
         self.model_name = "microsoft/DialoGPT-small"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.model = AutoModelForCausalLM.from_pretrained(self.model_name)
+    
+    def encode_prompt(self, prompt: str):
+        return self.tokenizer(prompt, return_tensors="pt")
+
     
 
 
