d97e7c0a4646a0504d1c6ed354a7f8348a2fea61
Leonie von Schirach <leonie.schirach@web.de>
2025-11-21 15:39:30 +0000

    test: check if reset_history method works as intended

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ from chatbot import Chatbot
 
 bot = Chatbot()
 
-encoded = bot.encode_prompt("Hello, how are you?")
-print(encoded)
+# encoded = bot.encode_prompt("Hello, how are you?")
+# print(encoded)
 
-decoded_prompt = bot.decode_reply([15496, 11, 703, 389, 345, 30]) # Pass in list of token IDs generated from tokenizer
-print(decoded_prompt)
+# decoded_prompt = bot.decode_reply([15496, 11, 703, 389, 345, 30]) # Pass in list of token IDs generated from tokenizer
+# print(decoded_prompt)
 
-prompt = "What is the weather like today?"
-reply = bot.generate_reply(prompt)
-print(f"Prompt: {prompt}")
-print(f"Reply: {reply}")
+# prompt = "What is the weather like today?"
+# reply = bot.generate_reply(prompt)
+# print(f"Prompt: {prompt}")
+# print(f"Reply: {reply}")
+
+# print(bot.chat_history_ids)
+
+bot.reset_history()
+
+prompts = [
+    "What's your name?",
+    "What do you think about AI?",
+    "Sorry, tell me your name again."
+]
+
+for prompt in prompts:
+    reply = bot.generate_reply(prompt)
+    print(f"Prompt: {prompt}")
+    print(f"Reply: {reply}\n")
+
+print(bot.chat_history_ids)
