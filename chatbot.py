daac1d08edab3fc7994dcfa2ba9cbea114ce090f
Leonie von Schirach <leonie.schirach@web.de>
2025-11-21 14:12:48 +0000

    feat: add generate_reply response to allow chatbot to reply to a prompt

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
 class Chatbot:
     
@@ -12,6 +13,25 @@ class Chatbot:
     
     def decode_reply(self, reply_ids: list[int]) -> str:
         return self.tokenizer.decode(reply_ids, skip_special_tokens=True)
+    
+    def generate_reply(self, prompt: str) -> str:
+        
+        prompt = prompt + "\n"
+        encoded_prompt = self.encode_prompt(prompt)
+        input_ids = encoded_prompt["input_ids"]
+
+        output = self.model.generate(
+            input_ids, 
+            pad_token_id=self.tokenizer.eos_token_id,
+            max_new_tokens=20,
+            do_sample=True, 
+            temperature=0.9, 
+            top_p=0.8, 
+            top_k=50
+            )
+        reply_token_ids = output[:, input_ids.shape[-1]:]
+
+        return str(self.decode_reply(reply_token_ids[0]))
 
 
     
